30c392e5c343cd79ba301436eca31cc2c053102d
PabloCarrai <pablohernanjuan@hotmail.com>
2026-09-16 14:53:15 -0300

    agregando switch case

--- gestor_db.sh
+++ gestor_db.sh
@@ -9,9 +9,45 @@ traer_db(){
     mariadb -h "$HOST" -u"$USUARIO" -p"$CLAVE" --skip-ssl -e "show databases;" 
 }
 
+MENU=$(cat << 'EOF'
+====================================
+      MENÚ DE OPCIONES
+====================================
+1) Mostrar DBs
+2) ???
+3) ???
+====================================
+EOF
+)
+
+
+
+
+
 if [ -f $ARCHIVO ]; then
-    echo "Dbs en Existencia:"
-    traer_db
+    echo $MENU
+    read entorno
+
+    # 2. El 'case' corre dentro del 'if'
+    case $entorno in
+        1)
+
+            echo "Dbs en Existencia:"
+            traer_db
+
+
+            ;;
+        2)
+            echo "Conectando al servidor de Pruebas..."
+            ;;
+        3)
+            echo "¡Alerta! Conectando al servidor de Producción..."
+            ;;
+        *)
+            echo "Entorno desconocido."
+            ;;
+    esac
+
 else
     echo "El archivo no existe."
 fi
