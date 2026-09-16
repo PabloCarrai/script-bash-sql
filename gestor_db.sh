c9ccd874a859233e1f70d6c67ce47448f5b58a2c
PabloCarrai <pablohernanjuan@hotmail.com>
2026-09-16 14:19:49 -0300

    ahi veremos si anda

---
+++ gestor_db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#   Ruta absoluta al archivo de credenciales.env
+ARCHIVO="$(dirname "$(readlink -f "$0")")/credenciales.env"
+#   Traigo las credenciales
+source $ARCHIVO
+
+traer_db(){
+    mariadb -h "$HOST" -u"$USUARIO" -p"$CLAVE" -e "show databases;"
+}
+
+if [ -f $ARCHIVO ]; then
+    echo "Aca traeria las db existentes"
+    traer_db
+else
+    echo "El archivo no existe."
+fi
